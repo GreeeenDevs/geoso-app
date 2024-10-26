86982a2b0792a551310b33e89e59de9e161ea236
hideakifukami <denys.fukami@gmail.com>
2024-10-26 19:15:15 -0300

    Criação de repositório para integração de front e backend.

---
+++ backend/src/test/java/br/greeeen/geoso/GeosoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.greeeen.geoso;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeosoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
